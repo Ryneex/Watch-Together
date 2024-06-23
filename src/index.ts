f8284e4c5d8d726bf41e5e2af8384d22713f67ff
MI Sajid <misajidsa@gmail.com>
2024-06-23 16:58:11 +0600

    feat: create socket events

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,76 @@
-import path from "path";
-import express from "express";
-const app = express();
+import { getRoomFromSocketID } from "./helpers/getRoomFromSocketID";
+import { getRoomsWithoutPrivateInfo } from "./helpers/getRoomsWithoutPrivateInfo";
+import { removeRoomIfNoActivity } from "./helpers/removeRoomIfNoActivity";
+import { removeUserFromRooms } from "./helpers/removeUserFromRooms";
+import { rooms } from "./rooms";
+import { io, server } from "./server";
+import { nameValidator } from "./validations/name.validator";
+import { v4 } from "uuid";
 
-app.use("/assets", express.static(path.resolve("build/dist/assets")));
-
-app.get("/*", (_, res) => {
-    if (process.env.NODE_ENV === "production") {
-        return res.sendFile(path.resolve("build/dist/index.html"));
-    }
-    res.sendFile(path.resolve("src/html/not-production.html"));
+io.use((socket, next) => {
+    if (nameValidator.safeParse(socket.handshake.auth.name).error) return next(new Error("Invalid Name"));
+    next();
 });
 
-app.listen(3000, () => console.log("Server is running on http://localhost:3000"));
+io.on("connection", (socket) => {
+    socket.emit("rooms", getRoomsWithoutPrivateInfo());
+
+    socket.on("create", (name: string) => {
+        if (nameValidator.safeParse(name).error) return;
+        const id = v4();
+        rooms.push({
+            id,
+            name: name,
+            lastActivity: Date.now(),
+            users: [],
+        });
+        const interval: NodeJS.Timeout = setInterval(() => removeRoomIfNoActivity(id, interval), 30000);
+        io.emit("rooms", getRoomsWithoutPrivateInfo());
+    });
+
+    socket.on("joinRoom", async (id: string) => {
+        const room = rooms.find((e) => e.id === id);
+        if (!room) return socket.emit("redirectToHome");
+        removeUserFromRooms(socket);
+        room.lastActivity = Date.now();
+        await socket.join(id);
+        room.lastActivity = Date.now();
+        room.users.push({
+            socket_id: socket.id,
+            name: socket.handshake.auth.name,
+            timestamp: 0,
+        });
+        io.to(id).emit("roomInfo", room);
+    });
+
+    socket.on("updateTimestamp", (time: number) => {
+        const { room, userIndex } = getRoomFromSocketID(socket.id);
+        if (!room) return;
+        const user = room.users[userIndex];
+        user.timestamp = time;
+        socket.emit("roomInfo", room);
+    });
+
+    socket.on("setVideoUrl", (e: string) => {
+        const { room } = getRoomFromSocketID(socket.id);
+        if (!room) return;
+        room.videoUrl = e;
+        io.to(room.id).emit("roomInfo", room);
+    });
+
+    socket.on("syncEveryone", (time: number) => {
+        const { room } = getRoomFromSocketID(socket.id);
+        if (!room) return;
+        socket.to(room.id).emit("syncEveryone", time);
+    });
+
+    socket.on("leave", () => {
+        removeUserFromRooms(socket);
+    });
+
+    socket.on("disconnect", () => {
+        removeUserFromRooms(socket);
+    });
+});
+
+server.listen(3000, () => console.log("Server is running on http://localhost:3000"));
