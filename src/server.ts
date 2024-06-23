e657b614b1523213878a21ca785980c65be622a1
MI Sajid <misajidsa@gmail.com>
2024-06-23 16:03:51 +0600

    feat: export server instance

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import path from "path";
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+const app = express();
+const server = http.createServer(app);
+
+app.use("/assets", express.static(path.resolve("build/dist/assets")));
+
+app.get("/*", (_, res) => {
+    if (process.env.NODE_ENV === "production") {
+        return res.sendFile(path.resolve("build/dist/index.html"));
+    }
+    res.sendFile(path.resolve("src/html/not-production.html"));
+});
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+
+export { app, server, io };
